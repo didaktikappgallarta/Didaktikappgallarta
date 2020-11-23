cdbe527f962c3d89c513d4d4f311a90b962f5ffe
Miguel <miguelbv2000@gmail.com>
2020-11-23 11:39:38 +0100

    Pantalla de inicio

---
+++ app/src/main/java/com/example/didaktikapp/MapaActivity.java
@@ -0,0 +1,13 @@
+package com.example.didaktikapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MapaActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.crucigrama);
+
+    }
+}
