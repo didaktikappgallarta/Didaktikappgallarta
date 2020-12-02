6d1a2d5f1fcf33de5fff86ae66fe86fb3f964bf2
Julen <ortzietxe@gmail.com>
2020-12-02 13:27:41 +0100

    Nuevo intento puzzle

--- app/src/main/java/com/example/didaktikapp/PuzzleActivity.java
+++ app/src/main/java/com/example/didaktikapp/PuzzleActivity.java
@@ -1,159 +1,85 @@
 package com.example.didaktikapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
-import android.graphics.Bitmap;
-import android.graphics.Matrix;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
-import static java.lang.Math.abs;
-
-public class PuzzleActivity extends AppCompatActivity {
-
-    ArrayList<Bitmap> pieces;
-
+public class PuzzleActivity extends AppCompatActivity implements Runnable, View.OnTouchListener {
+    PuzzleLayout puzzleLayout;
+    TextView tvTips;
+    ImageView ivTips;
+    int squareRootNum = 2;
+    int drawableId = R.mipmap.zugaztieta_puzzlea;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle);
-
-        final RelativeLayout relativeLayout = findViewById(R.id.layout);
-
-        getSupportActionBar().hide();
-        final ConstraintLayout constraintLayout = findViewById(R.id.layout);
-        ImageView imageView = findViewById(R.id.imageView);
-
-        // run image related code after the view was laid out
-        // to have all dimensions calculated
-        imageView.post(new Runnable() {
+        ivTips = (ImageView) findViewById(R.id.iv_tips);
+        ivTips.setImageResource(drawableId);
+        tvTips = (TextView) findViewById(R.id.tv_tips);
+        tvTips.setOnTouchListener(this);
+        puzzleLayout = (PuzzleLayout) findViewById(R.id.activity_swipe_card);
+        puzzleLayout.setImage(drawableId, squareRootNum);
+        puzzleLayout.setOnCompleteCallback(new PuzzleLayout.OnCompleteCallback() {
             @Override
-            public void run() {
-                pieces = splitImage();
-                TouchListener touchListener = new TouchListener();
-                for(Bitmap piece : pieces) {
-                    ImageView iv = new ImageView(getApplicationContext());
-                    iv.setImageBitmap(piece);
-                    iv.setOnTouchListener(touchListener);
-                    constraintLayout.addView(iv);
-                }
+            public void onComplete() {
+                Toast.makeText(PuzzleActivity.this, R.string.next, Toast.LENGTH_LONG).show();
+                puzzleLayout.postDelayed(PuzzleActivity.this, 800);
             }
         });
     }
 
-    private int[] getBitmapPositionInsideImageView(ImageView imageView) {
-        int[] ret = new int[4];
-
-        if (imageView == null || imageView.getDrawable() == null)
-            return ret;
-
-        // Get image dimensions
-        // Get image matrix values and place them in an array
-        float[] f = new float[9];
-        imageView.getImageMatrix().getValues(f);
-
-        // Extract the scale values using the constants (if aspect ratio maintained, scaleX == scaleY)
-        final float scaleX = f[Matrix.MSCALE_X];
-        final float scaleY = f[Matrix.MSCALE_Y];
-
-        // Get the drawable (could also get the bitmap behind the drawable and getWidth/getHeight)
-        final Drawable d = imageView.getDrawable();
-        final int origW = d.getIntrinsicWidth();
-        final int origH = d.getIntrinsicHeight();
-
-        // Calculate the actual dimensions
-        final int actW = Math.round(origW * scaleX);
-        final int actH = Math.round(origH * scaleY);
-
-        ret[2] = actW;
-        ret[3] = actH;
-
-        // Get image position
-        // We assume that the image is centered into ImageView
-        int imgViewW = imageView.getWidth();
-        int imgViewH = imageView.getHeight();
-
-        int top = (int) (imgViewH - actH)/2;
-        int left = (int) (imgViewW - actW)/2;
-
-        ret[0] = left;
-        ret[1] = top;
-
-        return ret;
+    @Override
+    public void run() {
+        squareRootNum++;
+        drawableId++;
+        if(squareRootNum > 10){
+            Toast.makeText(PuzzleActivity.this, R.string.complete, Toast.LENGTH_SHORT).show();
+            showDialog();
+        }else {
+            ivTips.setImageResource(drawableId);
+            puzzleLayout.setImage(drawableId, squareRootNum);
+        }
     }
 
-    private ArrayList<Bitmap> splitImage() {
-        int piecesNumber = 12;
-        int rows = 4;
-        int cols = 3;
-
-        ImageView imageView = findViewById(R.id.imageView);
-        ArrayList<Bitmap> pieces = new ArrayList<>(piecesNumber);
-
-        // Get the scaled bitmap of the source image
-        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
-        Bitmap bitmap = drawable.getBitmap();
-
-        int[] dimensions = getBitmapPositionInsideImageView(imageView);
-        int scaledBitmapLeft = dimensions[0];
-        int scaledBitmapTop = dimensions[1];
-        int scaledBitmapWidth = dimensions[2];
-        int scaledBitmapHeight = dimensions[3];
-
-        int croppedImageWidth = scaledBitmapWidth - 2 * abs(scaledBitmapLeft);
-        int croppedImageHeight = scaledBitmapHeight - 2 * abs(scaledBitmapTop);
-
-        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, scaledBitmapWidth, scaledBitmapHeight, true);
-        Bitmap croppedBitmap = Bitmap.createBitmap(scaledBitmap, abs(scaledBitmapLeft), abs(scaledBitmapTop), croppedImageWidth, croppedImageHeight);
-
-        // Calculate the with and height of the pieces
-        int pieceWidth = croppedImageWidth/cols;
-        int pieceHeight = croppedImageHeight/rows;
-
-        // Create each bitmap piece and add it to the resulting array
-        int yCoord = 0;
-        for (int row = 0; row < rows; row++) {
-            int xCoord = 0;
-            for (int col = 0; col < cols; col++) {
-                pieces.add(Bitmap.createBitmap(croppedBitmap, xCoord, yCoord, pieceWidth, pieceHeight));
-                xCoord += pieceWidth;
-            }
-            yCoord += pieceHeight;
-        }
-
-        return pieces;
+    private void showDialog() {
+        new AlertDialog.Builder(PuzzleActivity.this)
+                .setTitle(R.string.success)
+                .setMessage(R.string.restart)
+                .setPositiveButton(R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                squareRootNum = 2;
+                                drawableId = R.mipmap.zugaztieta_puzzlea;
+                                ivTips.setImageResource(drawableId);
+                                puzzleLayout.setImage(drawableId, squareRootNum);
+                            }
+                        }).setNegativeButton(R.string.exit,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                }).show();
     }
 
-    public class TouchListener implements View.OnTouchListener {
-        private float xDelta;
-        private float yDelta;
-
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            float x = motionEvent.getRawX();
-            float y = motionEvent.getRawY();
-            RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-            switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
-                case MotionEvent.ACTION_DOWN:
-                    xDelta = x - lParams.leftMargin;
-                    yDelta = y - lParams.topMargin;
-                    break;
-                case MotionEvent.ACTION_MOVE:
-                    lParams.leftMargin = (int) (x - xDelta);
-                    lParams.topMargin = (int) (y - yDelta);
-                    view.setLayoutParams(lParams);
-                    break;
-            }
-
-            return true;
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                ivTips.setVisibility(View.VISIBLE);
+                break;
+            default:
+                ivTips.setVisibility(View.GONE);
         }
+        return true;
     }
 }
--- app/src/main/java/com/example/didaktikapp/puzzlePiece.java
+++
@@ -1,4 +0,0 @@
-package com.example.didaktikapp;
-
-public class puzzlePiece {
-}
