2c120a860ad73cd75d3664bb4d952bfe328c8bae
Miguel <miguelbv2000@gmail.com>
2020-12-04 09:12:56 +0100

    Ahorcado funcionando

--- app/src/main/java/com/example/didaktikapp/MemoramaActivity.java
+++ app/src/main/java/com/example/didaktikapp/MemoramaActivity.java
@@ -18,11 +18,11 @@ import java.util.Collections;
 
 public class MemoramaActivity extends AppCompatActivity {
 
-    ImageView iv_11, iv_12, iv_13, iv_14, iv_21, iv_22, iv_23, iv_24, iv_31, iv_32, iv_33, iv_34;
+    ImageView iv_11, iv_12, iv_13, iv_14, iv_21, iv_22, iv_23, iv_24;
 
-    Integer[] arrayImagenes = {101, 102, 103, 104, 105, 106, 201,202, 203, 204, 205, 206};
+    Integer[] arrayImagenes = {101, 102, 103, 104, 201,202, 203, 204};
 
-    int image101, image102, image103, image104, image105, image106, image201, image202, image203, image204, image205, image206;
+    int image101, image102, image103, image104, image201, image202, image203, image204;
 
     int firstCard, secondCard;
     int clickedFirst, clickedSecond;
@@ -43,10 +43,6 @@ public class MemoramaActivity extends AppCompatActivity {
         iv_22 = (ImageView) findViewById(R.id.iv_22);
         iv_23 = (ImageView) findViewById(R.id.iv_23);
         iv_24 = (ImageView) findViewById(R.id.iv_24);
-        iv_31 = (ImageView) findViewById(R.id.iv_31);
-        iv_32 = (ImageView) findViewById(R.id.iv_32);
-        iv_33 = (ImageView) findViewById(R.id.iv_33);
-        iv_34 = (ImageView) findViewById(R.id.iv_34);
 
         iv_11.setTag("0");
         iv_12.setTag("1");
@@ -56,10 +52,6 @@ public class MemoramaActivity extends AppCompatActivity {
         iv_22.setTag("5");
         iv_23.setTag("6");
         iv_24.setTag("7");
-        iv_31.setTag("8");
-        iv_32.setTag("9");
-        iv_33.setTag("10");
-        iv_34.setTag("11");
 
         cargarImagenes();
 
@@ -129,38 +121,6 @@ public class MemoramaActivity extends AppCompatActivity {
             }
         });
 
-        iv_31.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int theCard = Integer.parseInt((String) v.getTag());
-                doStuff(iv_31, theCard);
-            }
-        });
-
-        iv_32.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int theCard = Integer.parseInt((String) v.getTag());
-                doStuff(iv_32, theCard);
-            }
-        });
-
-        iv_33.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int theCard = Integer.parseInt((String) v.getTag());
-                doStuff(iv_33, theCard);
-            }
-        });
-
-        iv_34.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int theCard = Integer.parseInt((String) v.getTag());
-                doStuff(iv_34, theCard);
-            }
-        });
-
     }
 
     public void doStuff(ImageView iv, int card) {
@@ -172,10 +132,6 @@ public class MemoramaActivity extends AppCompatActivity {
             iv.setImageResource(image103);
         } else if (arrayImagenes[card] == 104) {
             iv.setImageResource(image104);
-        } else if (arrayImagenes[card] == 105) {
-            iv.setImageResource(image105);
-        } else if (arrayImagenes[card] == 106) {
-            iv.setImageResource(image106);
         } else if (arrayImagenes[card] == 201) {
             iv.setImageResource(image201);
         } else if (arrayImagenes[card] == 202) {
@@ -184,10 +140,6 @@ public class MemoramaActivity extends AppCompatActivity {
             iv.setImageResource(image203);
         } else if (arrayImagenes[card] == 204) {
             iv.setImageResource(image204);
-        } else if (arrayImagenes[card] == 205) {
-            iv.setImageResource(image205);
-        } else if (arrayImagenes[card] == 206) {
-            iv.setImageResource(image206);
         }
 
         if (cardNumber == 1) {
@@ -217,10 +169,6 @@ public class MemoramaActivity extends AppCompatActivity {
             iv_22.setEnabled(false);
             iv_23.setEnabled(false);
             iv_24.setEnabled(false);
-            iv_31.setEnabled(false);
-            iv_32.setEnabled(false);
-            iv_33.setEnabled(false);
-            iv_34.setEnabled(false);
 
             Handler handler = new Handler();
             handler.postDelayed(new Runnable() {
@@ -250,14 +198,6 @@ public class MemoramaActivity extends AppCompatActivity {
                 iv_23.setVisibility(View.INVISIBLE);
             } else if(clickedFirst == 7) {
                 iv_24.setVisibility(View.INVISIBLE);
-            } else if(clickedFirst == 8) {
-                iv_31.setVisibility(View.INVISIBLE);
-            } else if(clickedFirst == 9) {
-                iv_32.setVisibility(View.INVISIBLE);
-            } else if(clickedFirst == 10) {
-                iv_33.setVisibility(View.INVISIBLE);
-            } else if(clickedFirst == 11) {
-                iv_34.setVisibility(View.INVISIBLE);
             }
 
             if(clickedSecond == 0) {
@@ -276,14 +216,6 @@ public class MemoramaActivity extends AppCompatActivity {
                 iv_23.setVisibility(View.INVISIBLE);
             } else if(clickedSecond == 7) {
                 iv_24.setVisibility(View.INVISIBLE);
-            } else if(clickedSecond == 8) {
-                iv_31.setVisibility(View.INVISIBLE);
-            } else if(clickedSecond == 9) {
-                iv_32.setVisibility(View.INVISIBLE);
-            } else if(clickedSecond == 10) {
-                iv_33.setVisibility(View.INVISIBLE);
-            } else if(clickedSecond == 11) {
-                iv_34.setVisibility(View.INVISIBLE);
             }
 
         } else {
@@ -295,11 +227,6 @@ public class MemoramaActivity extends AppCompatActivity {
             iv_22.setImageResource(R.drawable.interrogacion);
             iv_23.setImageResource(R.drawable.interrogacion);
             iv_24.setImageResource(R.drawable.interrogacion);
-            iv_31.setImageResource(R.drawable.interrogacion);
-            iv_32.setImageResource(R.drawable.interrogacion);
-            iv_33.setImageResource(R.drawable.interrogacion);
-            iv_34.setImageResource(R.drawable.interrogacion);
-
         }
 
         iv_11.setEnabled(true);
@@ -310,10 +237,6 @@ public class MemoramaActivity extends AppCompatActivity {
         iv_22.setEnabled(true);
         iv_23.setEnabled(true);
         iv_24.setEnabled(true);
-        iv_31.setEnabled(true);
-        iv_32.setEnabled(true);
-        iv_33.setEnabled(true);
-        iv_34.setEnabled(true);
 
         checkEnd();
     }
@@ -326,11 +249,7 @@ public class MemoramaActivity extends AppCompatActivity {
                 iv_21.getVisibility() == View.INVISIBLE &&
                 iv_22.getVisibility() == View.INVISIBLE &&
                 iv_23.getVisibility() == View.INVISIBLE &&
-                iv_24.getVisibility() == View.INVISIBLE &&
-                iv_31.getVisibility() == View.INVISIBLE &&
-                iv_32.getVisibility() == View.INVISIBLE &&
-                iv_33.getVisibility() == View.INVISIBLE &&
-                iv_34.getVisibility() == View.INVISIBLE) {
+                iv_24.getVisibility() == View.INVISIBLE ) {
 
             AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MemoramaActivity.this);
             alertDialogBuilder
@@ -356,18 +275,13 @@ public class MemoramaActivity extends AppCompatActivity {
     }
 
     private void cargarImagenes() {
-        image101 = R.drawable.cerdo;
-        image102 = R.drawable.mapache;
-        image103 = R.drawable.caballo;
-        image104 = R.drawable.conejo;
-        image105 = R.drawable.perro;
-        image106 = R.drawable.gato;
-        image201 = R.drawable.cerdo;
-        image202 = R.drawable.mapache;
-        image203 = R.drawable.caballo;
-        image204 = R.drawable.conejo;
-        image205 = R.drawable.perro;
-        image206 = R.drawable.gato;
-
+        image101 = R.drawable.lehenengoirudia;
+        image102 = R.drawable.bigarrenirudia;
+        image103 = R.drawable.hirugarrenirudia;
+        image104 = R.drawable.laugarrenirudia;
+        image201 = R.drawable.lehenengoirudia;
+        image202 = R.drawable.bigarrenirudia;
+        image203 = R.drawable.hirugarrenirudia;
+        image204 = R.drawable.laugarrenirudia;
     }
 }
